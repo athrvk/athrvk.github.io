222b4689b4b18a19165b2d592b240d809ec89f5d
Atharva Kusumbia <45625606+athrvk@users.noreply.github.com>
2020-07-31 15:44:27 +0530

    Added Randomization, toTop btn

--- scripting.js
+++ scripting.js
@@ -1,35 +1,26 @@
-var img1hover = document.querySelector("#img1");
-var img4hover = document.querySelector("#img4");
-var img6hover = document.querySelector("#img6");
-
-img1hover.addEventListener('mouseover', function () {
-    document.querySelector("#aboutMeDiv").style.color ="White";
-    document.querySelector("#aboutMeHeading").style.color ="White";
-});
-
-img1hover.addEventListener('mouseout', function () {
-    document.querySelector("#aboutMeDiv").style.color = null;
-    document.querySelector("#aboutMeHeading").style.color = null;
-});
+var shuffleButton = document.getElementById("shuffleBtn");
 
 
-img4hover.addEventListener('mouseover', function () {
-    document.querySelector("#aboutMeDiv").style.color ="White";
-    document.querySelector("#aboutMeHeading").style.color ="White";
-});
+function shuffleImgs() {
+    for(var i=0;i<6;i++){
+        document.querySelectorAll("li.images")[i].style.order = String(Math.floor(Math.random()*7));
+    }
+}
 
-img4hover.addEventListener('mouseout', function () {
-    document.querySelector("#aboutMeDiv").style.color = null;
-    document.querySelector("#aboutMeHeading").style.color = null;
-});
+shuffleButton.addEventListener('click', shuffleImgs);
 
+var imgelem = [document.querySelector("#img1"),
+               document.querySelector("#img4"),
+               document.querySelector("#img6")];
 
-img6hover.addEventListener('mouseover', function () {
-    document.querySelector("#aboutMeDiv").style.color ="White";
-    document.querySelector("#aboutMeHeading").style.color ="White";
-});
+for(var i =0;i<imgelem.length;i++){
+    imgelem[i].addEventListener('mouseover', function () {
+        document.querySelector("#aboutMeDiv").style.color ="White";
+        document.querySelector("#aboutMeHeading").style.color ="White";
+    });
 
-img6hover.addEventListener('mouseout', function () {
-    document.querySelector("#aboutMeDiv").style.color = null;
-    document.querySelector("#aboutMeHeading").style.color = null;
-});
+    imgelem[i].addEventListener('mouseout', function () {
+        document.querySelector("#aboutMeDiv").style.color = null;
+        document.querySelector("#aboutMeHeading").style.color = null;
+    });
+}
